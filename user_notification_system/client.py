1be4be1efd7bf5d5565b8378b0b88d24d5c33789
breynisson <bjorgvin.reynisson@gmail.com>
2025-01-26 00:04:08 +0000

    Refactoring client.py

--- user_notification_system/client.py
+++ user_notification_system/client.py
@@ -28,7 +28,6 @@ class NotificationClient:
             logging.error(f"Failed to get status for client {client_id}: {e.details()} (code: {e.code()})")
 
     def get_all_client_statuses(self):
-        # TODO: Debuug the ootput of this. Some strange stuff going on here. Encoding/Decoding?
         try:
             status_response = self.stub.GetAllClientStatuses(google.protobuf.empty_pb2.Empty())
             logging.info("All client statuses:")
@@ -68,41 +67,3 @@ def get_all_client_statuses():
 
     client = NotificationClient(args.server_address)
     client.get_all_client_statuses()
-
-def main():
-    import argparse
-    parser = argparse.ArgumentParser(description="Simulate multiple clients sending messages.")
-    parser.add_argument("--clients", type=int, default=1, help="Number of clients to simulate.")
-    parser.add_argument("--message", type=str, choices=["Hello", "Goodbye"], help="Message to send.")
-    parser.add_argument("--fetch-all-statuses", action="store_true", help="Fetch the statuses of all clients before and after sending messages.")
-    parser.add_argument("--server-status", action="store_true", help="Fetch the statuses of all clients without sending any messages.")
-    parser.add_argument("--server-address", type=str, default="localhost:50051", help="Address of the gRPC server.")
-
-    args = parser.parse_args()
-    num_clients = args.clients
-    message = args.message
-    fetch_all_statuses = args.fetch_all_statuses
-    server_status = args.server_status
-    server_address = args.server_address
-
-    client = NotificationClient(server_address)
-
-    if server_status:
-        client.get_all_client_statuses()
-        return
-
-    if fetch_all_statuses:
-        client.get_all_client_statuses()
-
-    if message:
-        from concurrent import futures
-        with futures.ThreadPoolExecutor(max_workers=num_clients) as executor:
-            for i in range(1, num_clients + 1):
-                client_id = f"client_{i}"
-                executor.submit(client.send_message, client_id, message)
-
-    if fetch_all_statuses:
-        client.get_all_client_statuses()
-
-if __name__ == "__main__":
-    main()
